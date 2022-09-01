dd95999bec160d0c95d298c7a35b310b258cfad1
Jeremiah Russell <jerry@jrussell.ie>
2022-09-01 06:36:42 +0100

    refactor(test): refine RUST_VEFRSION check
    only one entry required for grep of rust version

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-set +ex
+set -ex
 
 rustup show | grep stable
 rustup show | grep beta
 rustup show | grep nightly
 rustup show | grep "${MIN_RUST}"
-rustup show | grep "${RUST_VERSION}"
+rustup show | grep -m 1 "${RUST_VERSION}"
 cargo release --version
 nextsv --version
