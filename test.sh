282918dcc7e36256ada17856fdf826cb63da5409
Jeremiah Russell <jerry@jrussell.ie>
2024-11-28 14:50:01 +0000

    chore: update test script to include additional version checks and directory listing

--- test.sh
+++ test.sh
@@ -7,5 +7,15 @@ rustup show | grep beta
 rustup show | grep nightly
 rustup show | grep "${MIN_RUST}"
 rustup show | grep -m 1 "${RUST_VERSION}"
+echo "-----------------------------------------"
 cargo release --version
+cargo audit --version
+cargo llvm-cov --version
+cargo nextest --version
+cargo expand --version
 nextsv --version
+pcu --version
+circleci-junit-fix --version
+echo "-----------------------------------------"
+ls -l /usr/local/cargo/bin/*
+echo "-----------------------------------------"
